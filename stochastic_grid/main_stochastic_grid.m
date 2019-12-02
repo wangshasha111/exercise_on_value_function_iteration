d0ee3cc29f21036753dbb23263d4335800e8b732
wangshasha111 <55397038+wangshasha111@users.noreply.github.com>
2019-12-01 22:59:43 -0500

    stochastic grid - to run on lab computer

--- stochastic_grid/main_stochastic_grid.m
+++ stochastic_grid/main_stochastic_grid.m
@@ -97,7 +97,6 @@ Nk = 500;
 vGrid_k = linspace(kMin,kMax,Nk)';
 inputs.vGrid_k = vGrid_k;
 
-%% Step 3. Pre-Iteration: Set labor to steady state
 
 %% Draw random grid points
 
@@ -110,11 +109,14 @@ inputs.vGrid_k = vGrid_k;
 % That means the other dimension also needs to be fixed. 
 % So we are going to take draws from different level of capital as well as from different shocks.
 
+% Note that we want to keep the first and the last grid point of capital
 NkDraws = ceil(Nk/100);
+NkStochasticGrid = NkDraws + 2;
 seedKDraws=rng;
 rng(seedKDraws);
-ikStochasticGrid = randperm(Nk,NkDraws);%a ROW vector containing numberOfDraws unique integers selected randomly from 1 to Nk inclusive.
+ikStochasticGrid = randperm(Nk-2,NkDraws)+1;%a ROW vector containing numberOfDraws unique integers selected randomly from 2 to Nk-1 inclusive.
 ikStochasticGrid = sort(ikStochasticGrid);
+ikStochasticGrid = [1,ikStochasticGrid,Nk];
 
 NaDraws = ceil(Na/2);
 seedADraws=rng;
@@ -129,6 +131,297 @@ for iaPrime = iaStochasticGrid
 end
 % Then normalize it to a proper probability matrix
 mProb_a1a2StochasticGrid = mProb_a1a2StochasticGrid./sum(mProb_a1a2StochasticGrid,2);
+
+
+%% Efficiency Matrices to save time
+% Because fsolve takes a long time in iteration to solve for labor_1,
+% labor_2, I create the matrices for them as well as for consumption_1 and
+% consumption_2 to retrieve from later.
+
+% Note we have to put "kPrime" and "k" on the first two dimensions in order not
+% to cause any problem of dimensionality during interpolation
+
+mLabor_1Fsolve = zeros(Nk,Nk,Na); % kPrime,k,[a_1,a_2]
+mLabor_2Fsolve = zeros(Nk,Nk,Na);
+mConsumption_1Fsolve = zeros(Nk,Nk,Na);
+mConsumption_2Fsolve = zeros(Nk,Nk,Na);
+mCurrentUtilityFsolve = zeros(Nk,Nk,Na);
+% mMarginalUtilityTodayFsolve = zeros(Nk,Na,Nk);
+% mMarginalUtilityTomorrowFsolve = ;
+
+% laborInitial=[labor_1_SteadyState,labor_2_SteadyState];
+opts1 = optimoptions('fsolve','Tolx',1e-6, 'Display','off');
+options = optimset('Display', 'off');
+
+tic
+for ia = 1:Na
+    a_1 = mGrid_a1a2(ia,1);
+    a_2 = mGrid_a1a2(ia,2);
+    
+    
+    parfor ik = 1:Nk
+        laborInitial=[labor_1_SteadyState,labor_2_SteadyState];
+        k = vGrid_k(ik);
+        
+        for ikPrime = 1:Nk
+            kPrime = vGrid_k(ikPrime);
+            
+            vLaborFsolve = fsolve(@(labor) laborFunction(labor,a_1,a_2,k,kPrime,mmu_1,mmu_2,aalphaK,aalphaL,ddelta), laborInitial,opts1);
+%                 [labor, fval] = fminbnd(@(labor) ...
+%                     -laborFunction(labor,a_1,a_2,k,kPrime,mmu_1,mmu_2,aalphaK,aalphaL,ddelta),...
+%                     labor_1_SteadyState,labor_2_SteadyState,options)
+            if isreal(vLaborFsolve)==0
+                break
+            end
+            mLabor_1Fsolve(ikPrime,ik,ia) = vLaborFsolve(1);
+            mLabor_2Fsolve(ikPrime,ik,ia) = vLaborFsolve(2);
+            mConsumption_1Fsolve(ikPrime,ik,ia) = consumptionFunction1(a_1,k,kPrime,vLaborFsolve(1),aalphaK,aalphaL,ddelta);
+            mConsumption_2Fsolve(ikPrime,ik,ia) = consumptionFunction2(a_2,vLaborFsolve(2));
+            mCurrentUtilityFsolve(ikPrime,ik,ia) = utilityFunction(mConsumption_1Fsolve(ikPrime,ik,ia),mConsumption_2Fsolve(ikPrime,ik,ia),vLaborFsolve(1),vLaborFsolve(2),mmu_1,mmu_2);
+%             mMarginalUtilityTodayFsolve(ikPrime,ia,ik) = mmu_1 * (mConsumption_2Fsolve(ikPrime,ia,ik))^mmu_2 * (mConsumption_1Fsolve(ikPrime,ia,ik))^(mmu_1-1);
+            laborInitial=[vLaborFsolve(1),vLaborFsolve(2)];
+        end
+        
+    end
+    
+    fprintf('Progress calculating period utility = %d%% \n', round(ia/Na*100)); 
+
+end
+toc
+elapsedTimeMinutes=toc/60;
+
+inputs.mLabor_1Fsolve = mLabor_1Fsolve;
+inputs.mLabor_2Fsolve = mLabor_2Fsolve;
+inputs.mConsumption_1Fsolve = mConsumption_1Fsolve;
+inputs.mConsumption_2Fsolve = mConsumption_2Fsolve;
+inputs.mCurrentUtilityFsolve = mCurrentUtilityFsolve;
+% inputs.mMarginalUtilityTodayFsolve = mMarginalUtilityTodayFsolve;
+% 
+% mLabor_1Fsolve=permute(mLabor_1Fsolve,[3,2,1]);
+% mLabor_2Fsolve=permute(mLabor_2Fsolve,[3,2,1]);
+% mConsumption_1Fsolve=permute(mConsumption_1Fsolve,[3,2,1]);
+% mConsumption_2Fsolve=permute(mConsumption_2Fsolve,[3,2,1]);
+% mCurrentUtilityFsolve=permute(mCurrentUtilityFsolve,[3,2,1]);
+
+save('efficiencyMatricesNk250','mLabor_1Fsolve','mLabor_2Fsolve','mConsumption_1Fsolve','mConsumption_2Fsolve','mCurrentUtilityFsolve','elapsedTimeMinutes')
+% 历时 2333.831306s 秒。% my computer
+% 历时 2152.400222 秒。 % my computer parfor 2019-11-30 22:15:53
+
+% Elapsed time is 385.744013 seconds. % Lab computer
+
+
+%% Then do the regular Value Function Iteration using value function calculated above as the first guess
+
+%% Required matrices and vectors
+
+mValue0                 = utilitySteadyState.*ones(NkStochasticGrid,Na); inputs.mValue0 = mValue0;
+mValue                  = zeros(NkStochasticGrid,Na);
+mKPolicy                = zeros(NkStochasticGrid,Na);
+mLaborPolicy_1          = zeros(NkStochasticGrid,Na);
+mLaborPolicy_2          = zeros(NkStochasticGrid,Na); 
+mConsumptionPolicy_1    = zeros(NkStochasticGrid,Na);
+mConsumptionPolicy_2    = zeros(NkStochasticGrid,Na);
+
+maxIter = 10000;
+tolerance     = 1e-6;
+
+iteration       = 1;
+mDifference = zeros(maxIter,1);
+mDifference(iteration) = 100;
+
+options = optimset('Display', 'off');
+opts1 = optimoptions('fsolve','Tolx',1e-6, 'Display','off');
+laborInitial=[labor_1_SteadyState,labor_2_SteadyState];
+
+tic
+
+% Finding value and policy functions numerically
+while iteration <= maxIter  &&  (mDifference(iteration) > tolerance)
+        
+    expectedValue0 = mValue0 * mProb_a1a2StochasticGrid';% row: kPrime, column: a, i.e., expected value if today's shock is a and I choose k as tomorrow's capital stock
+    
+
+    for ia = 1:Na
+        a_1 = mGrid_a1a2(ia,1);
+        a_2 = mGrid_a1a2(ia,2);
+
+        iikPrime = 1;
+
+        for ik = ikStochasticGrid(1:end)
+
+            k = vGrid_k(ik);
+
+            valueHighSoFar = -1000.0;
+            kChoice  = vGrid_k(1);
+%                     laborInitial=[labor_1_SteadyState,labor_2_SteadyState];
+
+            for ikPrime = ikStochasticGrid(iikPrime:end)
+                kPrime = vGrid_k(ikPrime);
+
+                [valueProvisional,labor_1,labor_2,consumption_1,consumption_2] = valueFunction_stochasticGrid...
+                    (kPrime,ikPrime,ik,k,ia,a_1,a_2,expectedValue0,bbeta,mmu_1,mmu_2,ddelta,aalphaK,aalphaL,laborInitial);
+
+                if (valueProvisional>valueHighSoFar)
+                    valueHighSoFar = valueProvisional;
+                    kChoice = vGrid_k(ikPrime);
+                    iikPrime = sum(ikPrime >= ikStochasticGrid);
+%                             laborInitial = [labor_1,labor_2];
+                    mValue(ik,ia) = valueHighSoFar;
+                    mKPolicy(ik,ia) = kChoice;
+                    mLaborPolicy_1(ik,ia) = labor_1;
+                    mLaborPolicy_2(ik,ia) = labor_2;
+                    mConsumptionPolicy_1(ik,ia) = consumption_1;
+                    mConsumptionPolicy_2(ik,ia) = consumption_2;
+                else
+                    break
+                end
+
+            end
+        end
+    end
+    iteration = iteration + 1;
+    mDifference(iteration) = max(abs(mValue - mValue0),[],'all');
+    mValue0         = mValue;
+
+%         if mod(iteration,10) == 2
+    fprintf(' Iteration: %2.0f, Sup diff: %2.8f\n', iteration-1, mDifference(iteration)); 
+%         end
+
+end
+
+toc
+
+fprintf(' Convergence achieved. Total Number of Iteration: %2.0f, Sup diff: %2.8f\n', iteration-1, mDifference(iteration)); 
+
+save ShashaWang_JFV_PS1_500_stochastic_capital_grid_points_valueFunctionIteration
+
+
+%% For accuracy test, compute the euler equation error
+
+% Let's use LINEAR INTERPOLATION for tomorrow's values
+
+errorEulerEquationLinearInterpolation = eulerEquationErrorFunction(Nk,vGrid_k,mKPolicy,mLaborPolicy_1,mConsumptionPolicy_1,mConsumptionPolicy_2,Na,mGrid_a1a2,mProb_a1a2,bbeta,mmu_1,mmu_2,ddelta,aalphaK,aalphaL);
+errorEulerEquationLinearInterpolationDecimalLog = log10( errorEulerEquationLinearInterpolation );
+
+[kk,aa]=meshgrid(vGrid_k, mGrid_a1a2(:,1));
+
+figure
+mesh(kk, aa, errorEulerEquationLinearInterpolationDecimalLog');
+
+title('Euler Equation Error $log_{10}$ Linear Interpolation - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('error','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_eulerEquationErrorLinearInterpolation_stochastic_grid')
+
+
+% Lab computer Thanksgiving day 11.28.2019 8:53pm
+%  Iteration:  1, Sup diff: 0.00156838
+%  Iteration: 11, Sup diff: 0.00059008
+%  Iteration: 21, Sup diff: 0.00038928
+%  Iteration: 31, Sup diff: 0.00025684
+%  Iteration: 41, Sup diff: 0.00016949
+%  Iteration: 51, Sup diff: 0.00011188
+%  Iteration: 61, Sup diff: 0.00007387
+%  Iteration: 71, Sup diff: 0.00004879
+%  Iteration: 81, Sup diff: 0.00003223
+%  Iteration: 91, Sup diff: 0.00002130
+%  Iteration: 101, Sup diff: 0.00001408
+%  Iteration: 111, Sup diff: 0.00000931
+%  Iteration: 121, Sup diff: 0.00000616
+%  Iteration: 131, Sup diff: 0.00000408
+%  Iteration: 141, Sup diff: 0.00000270
+%  Iteration: 151, Sup diff: 0.00000179
+%  Iteration: 161, Sup diff: 0.00000118
+% Elapsed time is 10393.859303 seconds.
+%  Convergence achieved. Total Number of Iteration: 167, Sup diff: 0.00000092
+
+%% figures for Value Function Iteration with a Fixed Grid
+
+figure
+mesh(kk, aa, mValue');
+
+title('Value - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Value','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_value_stochastic_grid')
+
+figure
+mesh(kk, aa, mKPolicy');
+
+title('Policy for Next Period Capital - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Next Period Capital $k\prime$','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_kPolicy_stochastic_grid')
+
+figure
+mesh(kk, aa, mLaborPolicy_1');
+
+title('Policy for Good 1 Labor - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Good 1 Labor','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_laborPolicy_1_stochastic_grid')
+
+figure
+mesh(kk, aa, mLaborPolicy_2');
+
+title('Policy for Good 2 Labor - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Good 2 Labor','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_laborPolicy_2_stochastic_grid')
+
+figure
+mesh(kk, aa, mConsumptionPolicy_1');
+
+title('Policy for Good 1 Consumption - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Good 1 Consumption','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_consumptionPolicy_1_stochastic_grid')
+
+figure
+mesh(kk, aa, mConsumptionPolicy_2');
+
+title('Policy for Good 2 Consumption - Stochastic Grid','interpreter','latex')
+xlabel('Capital Stock $k$','interpreter','latex')
+ylabel('shocks $z_1$ $z_2$','interpreter','latex')
+zlabel('Good 2 Consumption','interpreter','latex')
+xlim([min(vGrid_k),max(vGrid_k)])
+ylim([min(mGrid_a1a2(:,1)),max(mGrid_a1a2(:,1))])
+savefig('q3_consumptionPolicy_2_stochastic_grid')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Step 3. Pre-Iteration: Set labor to steady state
 
 %% Required matrices and vectors for Pre-Iteration
 
