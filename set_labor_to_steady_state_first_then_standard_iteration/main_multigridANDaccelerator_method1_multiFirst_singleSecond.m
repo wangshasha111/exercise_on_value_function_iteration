e7862e3a23ba2270ea21d792ebc30c53208ec7ba
wangshasha111 <55397038+wangshasha111@users.noreply.github.com>
2019-12-01 20:16:55 -0500

    running multigrid accelerator method 1
     on lab computer

--- set_labor_to_steady_state_first_then_standard_iteration/main_multigridANDaccelerator_method1_multiFirst_singleSecond.m
+++ set_labor_to_steady_state_first_then_standard_iteration/main_multigridANDaccelerator_method1_multiFirst_singleSecond.m
@@ -234,49 +234,37 @@ fprintf(' Convergence Achieved. \n Number of Grid Points: %2.0f\n Iteration: %2.
 
 save ShashaWang_JFV_PS1_250_capital_grid_points_valueFunctionIteration_setLaborToSteadyState_multigridANDaccelerator_method1
 
-%  Iteration:  1, Sup diff: 0.00530891
-%  Iteration: 11, Sup diff: 0.00073227
-%  Iteration: 21, Sup diff: 0.00019339
-%  Iteration: 31, Sup diff: 0.00010574
-%  Iteration: 41, Sup diff: 0.00011728
-%  Iteration: 51, Sup diff: 0.00003955
-%  Iteration: 61, Sup diff: 0.00002003
-%  Iteration: 71, Sup diff: 0.00001179
-%  Iteration: 81, Sup diff: 0.00000718
-%  Iteration: 91, Sup diff: 0.00000440
-%  Iteration: 101, Sup diff: 0.00000271
-%  Iteration: 111, Sup diff: 0.00000167
-%  Iteration: 121, Sup diff: 0.00000104
-%  Iteration: 123, Sup diff: 0.00000094
+% lab computer 12/1/2019
+%  Iteration:  1, Sup diff: 0.00862455
+%  Iteration: 11, Sup diff: 0.00122077
+%  Iteration: 21, Sup diff: 0.00032535
+%  Iteration: 31, Sup diff: 0.00016376
+%  Iteration: 41, Sup diff: 0.00014885
+%  Iteration: 51, Sup diff: 0.00008655
+%  Iteration: 61, Sup diff: 0.00003109
+%  Iteration: 71, Sup diff: 0.00001889
+%  Iteration: 81, Sup diff: 0.00001151
+%  Iteration: 91, Sup diff: 0.00000704
+%  Iteration: 101, Sup diff: 0.00000432
+%  Iteration: 111, Sup diff: 0.00000266
+%  Iteration: 121, Sup diff: 0.00000164
+%  Iteration: 131, Sup diff: 0.00000102
+%  Iteration: 133, Sup diff: 0.00000093
 %  Convergence Achieved. 
 %  Number of Grid Points: 100
-%  Iteration: 123, Sup difference: 0.00000094
-
-%  Iteration:  1, Sup diff: 0.00000091
+%  Iteration: 133, Sup difference: 0.00000093
+%   Iteration:  1, Sup diff: 0.00000090
 %  Convergence Achieved. 
 %  Number of Grid Points: 500
-%  Iteration:  1, Sup difference: 0.00000091
- 
-%  Iteration:  1, Sup diff: 0.00046054
-%  Iteration: 11, Sup diff: 0.00029520
-%  Iteration: 21, Sup diff: 0.00022697
-%  Iteration: 31, Sup diff: 0.00016110
-%  Iteration: 41, Sup diff: 0.00025614
-%  Iteration: 51, Sup diff: 0.00038761
-%  Iteration: 61, Sup diff: 0.00014773
-%  Iteration: 71, Sup diff: 0.00011937
-%  Iteration: 81, Sup diff: 0.00015672
-%  Iteration: 91, Sup diff: 0.00014987
-%  Iteration: 93, Sup diff: 0.00000093
+%  Iteration:  1, Sup difference: 0.00000090
+%   Iteration:  1, Sup diff: 0.00000085
 %  Convergence Achieved. 
 %  Number of Grid Points: 5000
-%  Iteration: 93, Sup difference: 0.00000093
-
-%  Elapsed time is 1358.047397 seconds.
-%  Convergence achieved. Total Number of Iteration: 93, Sup diff: 0.00000093
+%  Iteration:  1, Sup difference: 0.00000085
+%  Elapsed time is 190.702722 seconds.
 %  Convergence Achieved. 
 %  Number of Grid Points: 5000
-%  Iteration: 93, Sup difference: 0.00000093
+%  Iteration:  1, Sup difference: 0.00000085
 
 
 %% Then do the real Value Function Iteration using value function calculated above as the first guess
