c52a1f04b38c239b129ae2b4be7eec8cf35dec6b
wangshasha111 <55397038+wangshasha111@users.noreply.github.com>
2019-12-10 16:24:27 -0500

    reorganize

--- homework_2/FelipeRMazin_HW2_codes/sstate.m
+++
@@ -1,4 +0,0 @@
-function f = sstate(x,laborSteadyState)
-
-f(1) = 1 - 0.99*(0.3*x(1)^(0.3-1)*laborSteadyState^0.7 + 0.9);
-f(2) = 0.7 * x(1)^0.3 * laborSteadyState^(-0.3) - (x(1)^0.3 * laborSteadyState^0.7 - 0.1 * x(1)) * x(2) * laborSteadyState;
